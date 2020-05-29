bc9e578b482ec45cf78d60f3264143851c0ed52d
Yasmen Abdelraheem <61633698+yasmenraheem@users.noreply.github.com>
2020-05-29 18:51:15 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,31 @@
 
+let aboutOffset = ($("#about").offset().top); // 619px
 
 
 $(window).scroll(function(){
-    console.log("hello")
+      
+    let wScroll = $(window).scrollTop()
+
+    if( wScroll > aboutOffset)
+      {
+        $("#btnUp").fadeIn(500)
+        $("nav").css("backgroundColor" , "rgba(0,0,0,0.6)");
+      }
+      else
+      {
+        $("#btnUp").fadeOut(500)
+        $("nav").css("backgroundColor" , "transparent");
+      }
+  
+})
+
+$("#btnUp").click(function(){
+   
+    $("html,body").animate({scrollTop:"0"} , 2000);
+})
+
+$("a").click(function(){   //#section id
+   let aHref = $(this).attr("href");
+   let sectionOffset = $(aHref).offset().top;
+   $("html,body").animate({scrollTop:sectionOffset} , 1500)
 })
